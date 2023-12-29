9a0a929c23ee290844b1a045738cc1ff79589e8c
Yunus Emre Gürdoğan <yunusemregurdogan0@gmail.com>
2023-12-29 17:16:11 +0300

    içerik sayfası

--- src/pages/gezilecek/Gezilecek.jsx
+++ src/pages/gezilecek/Gezilecek.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./gezilecek.css";
-import logo from "../../images/mercedesLogo.png";
+import logo from "../../images/selimiye.jpg";
 
 function Gezilecek() {
   return (
     <div className="body">
       <div className="content">
         <div className="title">
-          Tarihi
+          SELİMİYE CAMİ
         </div>
         <div className="image">
           <img src={logo}/>
@@ -15,14 +15,12 @@ function Gezilecek() {
         <div className="explanation">
         Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif minaresi, muhteşem kubbesi ile eşsiz bir yapı olan 
         Selimiye Camii dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın eseridir. 
-        Yapımına II.Selim’in emri ile 1568 yılında başlanan caminin inşası binlerce kişinin yoğun çalışması 
-        ile yedi yıl sürmüş ve 1575 yılında tamamlanmıştır. 
-        Osmanlı mimarisinin en önemli eseri olarak kabul edilen camiyi Mimar Sinan da “ustalık eserim” olarak tanımlamıştır.
+        <button className="button">Daha Fazla</button>
         </div>
       </div>
       <div className="content">
         <div className="title">
-          Tarihi
+          SELİMİYE CAMİ
         </div>
         <div className="image">
           <img src={logo}/>
@@ -30,53 +28,36 @@ function Gezilecek() {
         <div className="explanation">
         Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif minaresi, muhteşem kubbesi ile eşsiz bir yapı olan 
         Selimiye Camii dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın eseridir. 
-        Yapımına II.Selim’in emri ile 1568 yılında başlanan caminin inşası binlerce kişinin yoğun çalışması 
-        ile yedi yıl sürmüş ve 1575 yılında tamamlanmıştır. 
-        Osmanlı mimarisinin en önemli eseri olarak kabul edilen camiyi Mimar Sinan da “ustalık eserim” olarak tanımlamıştır.
-        </div>
-      </div><div className="content">
-        <div className="title">
-          Tarihi
-        </div>
-        <div className="image">
-          <img src={logo}/>
-        </div>
-        <div className="explanation">
-        Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif minaresi, muhteşem kubbesi ile eşsiz bir yapı olan 
-        Selimiye Camii dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın eseridir. 
-        Yapımına II.Selim’in emri ile 1568 yılında başlanan caminin inşası binlerce kişinin yoğun çalışması 
-        ile yedi yıl sürmüş ve 1575 yılında tamamlanmıştır. 
-        Osmanlı mimarisinin en önemli eseri olarak kabul edilen camiyi Mimar Sinan da “ustalık eserim” olarak tanımlamıştır.
-        </div>
-      </div><div className="content">
-        <div className="title">
-          Tarihi
-        </div>
-        <div className="image">
-          <img src={logo}/>
-        </div>
-        <div className="explanation">
-        Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif minaresi, muhteşem kubbesi ile eşsiz bir yapı olan 
-        Selimiye Camii dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın eseridir. 
-        Yapımına II.Selim’in emri ile 1568 yılında başlanan caminin inşası binlerce kişinin yoğun çalışması 
-        ile yedi yıl sürmüş ve 1575 yılında tamamlanmıştır. 
-        Osmanlı mimarisinin en önemli eseri olarak kabul edilen camiyi Mimar Sinan da “ustalık eserim” olarak tanımlamıştır.
-        </div>
-      </div><div className="content">
-        <div className="title">
-          Tarihi
-        </div>
-        <div className="image">
-          <img src={logo}/>
-        </div>
-        <div className="explanation">
-        Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif minaresi, muhteşem kubbesi ile eşsiz bir yapı olan 
-        Selimiye Camii dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın eseridir. 
-        Yapımına II.Selim’in emri ile 1568 yılında başlanan caminin inşası binlerce kişinin yoğun çalışması 
-        ile yedi yıl sürmüş ve 1575 yılında tamamlanmıştır. 
-        Osmanlı mimarisinin en önemli eseri olarak kabul edilen camiyi Mimar Sinan da “ustalık eserim” olarak tanımlamıştır.
+        <button className="button">Daha Fazla</button>
         </div>
       </div>
+      <div className="content">
+        <div className="title">
+          SELİMİYE CAMİ
+        </div>
+        <div className="image">
+          <img src={logo}/>
+        </div>
+        <div className="explanation">
+        Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif minaresi, muhteşem kubbesi ile eşsiz bir yapı olan 
+        Selimiye Camii dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın eseridir. 
+        <button className="button">Daha Fazla</button>
+        </div>
+      </div>
+      <div className="content">
+        <div className="title">
+          SELİMİYE CAMİ
+        </div>
+        <div className="image">
+          <img src={logo}/>
+        </div>
+        <div className="explanation">
+        Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif minaresi, muhteşem kubbesi ile eşsiz bir yapı olan 
+        Selimiye Camii dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın eseridir. 
+        <button className="button">Daha Fazla</button>
+        </div>
+      </div>
+      
     </div>
   );
 }
