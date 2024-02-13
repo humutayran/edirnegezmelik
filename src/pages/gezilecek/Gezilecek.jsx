3081275b4585b057c8b0cde4039b130f702fcd6c
Yunus Emre Gürdoğan <yunusemregurdogan0@gmail.com>
2024-02-14 01:26:19 +0300

    içerik sayfası listeleme animasyonları yapıldı

--- src/pages/gezilecek/Gezilecek.jsx
+++ src/pages/gezilecek/Gezilecek.jsx
@@ -1,69 +1,171 @@
 import React from "react";
-import "./gezilecek.css";
+import "../icerik.css";
 import karaagac from "../../images/karaagac.jpg";
 import kentormani from "../../images/edirnekentormani.jpg";
+import { motion } from "framer-motion";
 
 function Gezilecek() {
+  const variantsTitle = {
+    hidden: { opacity: 0, x: -25 },
+    visible: { opacity: 1, x: 0 },
+  };
+  const variants = {
+    hidden: { opacity: 0, y: 25 },
+    visible: { opacity: 1, y: 0 },
+  };
+  const transition = { duration: 0.5, delay: 0.25 };
+  const viewport = { once: true };
   return (
     <div className="body">
       <div className="container">
         <div className="container-left">
-          <div className="title">KARAAĞAÇ</div>
-          <div className="explanation">
-            Karaağaç, Edirne ilinin Merkez ilçesine bağlı bir mahalle. Karaağaç,
-            Meriç nehrinin 2 km batısında, Yunanistan sınırına 4 km
-            uzaklıktadır.
-          </div>
-          <button className="button">Daha Fazla</button>
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            KARAAĞAÇ
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">
+              Karaağaç, Edirne ilinin Merkez ilçesine bağlı bir mahalle.
+              Karaağaç, Meriç nehrinin 2 km batısında, Yunanistan sınırına 4 km
+              uzaklıktadır.
+            </div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
-        <div className="container-right">
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
           <img src={karaagac} />
-        </div>
+        </motion.div>
       </div>
       <div className="container">
         <div className="container-left">
-          <div className="title">Edirne Kent Ormanı</div>
-          <div className="explanation">
-            İl merkezinin güney kesminde Edirne-Karaağaç yolu üzerinde kente 2
-            kilometre mesafede olan bir mesire yeridir. Yüksek ağaçlarla kaplı
-            Meriç kıyısındaki bu alanda gazino, restoran, çocuk parkları, çay
-            bahçeleri, çeşitli eğlence ve spor olanakları sunan yerler vardır.
-          </div>
-          <button className="button">Daha Fazla</button>
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            Edirne Kent Ormanı
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">
+              İl merkezinin güney kesminde Edirne-Karaağaç yolu üzerinde kente 2
+              kilometre mesafede olan bir mesire yeridir.
+            </div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
-        <div className="container-right">
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
           <img src={kentormani} />
-        </div>
+        </motion.div>
       </div>
       <div className="container">
         <div className="container-left">
-          <div className="title">SELİMİYE CAMİ</div>
-          <div className="explanation">
-            Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif
-            minaresi, muhteşem kubbesi ile eşsiz bir yapı olan Selimiye Camii
-            dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın
-            eseridir.
-          </div>
-          <button className="button">Daha Fazla</button>
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            KARAAĞAÇ
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">
+              Karaağaç, Edirne ilinin Merkez ilçesine bağlı bir mahalle.
+              Karaağaç, Meriç nehrinin 2 km batısında, Yunanistan sınırına 4 km
+              uzaklıktadır.
+            </div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
-        <div className="container-right">
-          <img src={kentormani} />
-        </div>
-      </div>
-      <div className="container">
-        <div className="container-left">
-          <div className="title">SELİMİYE CAMİ</div>
-          <div className="explanation">
-            Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif
-            minaresi, muhteşem kubbesi ile eşsiz bir yapı olan Selimiye Camii
-            dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın
-            eseridir.
-          </div>
-          <button className="button">Daha Fazla</button>
-        </div>
-        <div className="container-right">
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
           <img src={karaagac} />
+        </motion.div>
+      </div>
+      <div className="container">
+        <div className="container-left">
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            Edirne Kent Ormanı
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">
+              İl merkezinin güney kesminde Edirne-Karaağaç yolu üzerinde kente 2
+              kilometre mesafede olan bir mesire yeridir.
+            </div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
+          <img src={kentormani} />
+        </motion.div>
       </div>
     </div>
   );
--- src/pages/tarihi/Tarihi.jsx
+++ src/pages/tarihi/Tarihi.jsx
@@ -1,50 +1,167 @@
 import React from "react";
-import "../gezilecek/gezilecek.css";
+import "../icerik.css";
 import selimiye from "../../images/selimiye.jpg";
 import adaletKasri from "../../images/adaletkasri.jpg";
+import { motion } from "framer-motion";
 
 function Tarihi() {
+  const variantsTitle = {
+    hidden: { opacity: 0, x: -25 },
+    visible: { opacity: 1, x: 0 },
+  };
+  const variants = {
+    hidden: { opacity: 0, y: 25 },
+    visible: { opacity: 1, y: 0 },
+  };
+  const transition = { duration: 0.5, delay: 0.25 };
+  const viewport = { once: true };
   return (
     <div className="body">
       <div className="container">
         <div className="container-left">
-          <div className="title">SELİMİYE CAMİİ</div>
-          <div className="explanation">dillerde tekbir AllahuEkber</div>
-          <button className="button">Daha Fazla</button>
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            SELİMİYE CAMİİ
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">
+              Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif
+              minaresi, muhteşem kubbesi ile eşsiz bir yapı olan Selimiye Camii
+              dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın
+              eseridir.
+            </div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
-        <div className="container-right">
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
           <img src={selimiye} />
-        </div>
+        </motion.div>
       </div>
       <div className="container">
         <div className="container-left">
-          <div className="title">ADALET KASRI</div>
-          <div className="explanation">dillerde tekbir AllahuEkber</div>
-          <button className="button">Daha Fazla</button>
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            ADALET KASRI
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">dillerde tekbir AllahuEkber</div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
-        <div className="container-right">
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
           <img src={adaletKasri} />
-        </div>
+        </motion.div>
       </div>
       <div className="container">
         <div className="container-left">
-          <div className="title">ADALET KASRI</div>
-          <div className="explanation">dillerde tekbir AllahuEkber</div>
-          <button className="button">Daha Fazla</button>
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            SELİMİYE CAMİİ
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">
+              Edirne’nin her yerinden tüm ihtişamı ile görülebilen, dört zarif
+              minaresi, muhteşem kubbesi ile eşsiz bir yapı olan Selimiye Camii
+              dünya tarihinin ünlü mimarlarından birisi olan Mimar Sinan’ın
+              eseridir.
+            </div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
-        <div className="container-right">
-          <img src={adaletKasri} />
-        </div>
-      </div>
-      <div className="container">
-        <div className="container-left">
-          <div className="title">SELİMİYE CAMİ</div>
-          <div className="explanation">dillerde tekbir AllahuEkber</div>
-          <button className="button">Daha Fazla</button>
-        </div>
-        <div className="container-right">
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
           <img src={selimiye} />
+        </motion.div>
+      </div>
+      <div className="container">
+        <div className="container-left">
+          <motion.div
+            className="title"
+            variants={variantsTitle}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            ADALET KASRI
+          </motion.div>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={viewport}
+            transition={transition}
+          >
+            <div className="explanation">dillerde tekbir AllahuEkber</div>
+            <button className="button">Daha Fazla</button>
+          </motion.div>
         </div>
+        <motion.div
+          className="container-right"
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+          transition={transition}
+        >
+          <img src={adaletKasri} />
+        </motion.div>
       </div>
     </div>
   );
