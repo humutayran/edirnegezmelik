e897a5e7c3c90a4dc03eca87290f293e440cd010
Yunus Emre Gürdoğan <yunusemregurdogan0@gmail.com>
2024-05-08 18:30:09 +0300

    içerik sayfası tasarımı tamamlandı

--- src/pages/icerik/Icerik.jsx
+++ src/pages/icerik/Icerik.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import styles from "./Icerik.module.css";
+import resim from "../../images/adalet-kasri.jpg";
 function Icerik() {
   return (
     <div className={styles.body}>
+      <div className={styles.title}>SELİMİYE CAMİİ</div>
       <div className={styles.container}>
-        <div className={styles.title}>SELİMİYE CAMİİ</div>
-        <div className={styles.container_left}></div>
-        <div className={styles.container_right}></div>
+        <div style={{display:"flex",flexDirection:"row",justifyContent:"space-evenly"}}>
+        <div className={styles.container_text} >
+        Mimar Sinan‘ın 80 yaşında inşa ettiği ve “ustalık eserim“ diye nitelediği yapıt olan Selimiye Camii, Osmanlı-Türk mimarlık tarihinin olduğu kadar, dünya mimarlık tarihinin de başyapıtları arasında gösterilmektedir. Edirnenin ve aynı zamanda Osmanlı Döneminin simgesi olan cami; 1569-1575 yılları arasında II. Selim’in emriyle yaptırılmıştır. Çok uzaklardan dört minaresi ile göze çarpan yapı, kurulduğu yerin seçimiyle Mimar Sinan’ın aynı zamanda usta bir şehircilik uzmanı olduğunu da gösterir. Kesme taştan yapılan cami iç bölümüyle 1620 metrekare, tümüyle ise toplam 2475 metrekarelik bir alanı kaplamaktadır. Selimiye Camii, yerden yüksekliği 43.28 metre olan 31.30 metre çapındaki kubbesiyle ilgi çeker.<br />    
+   Kubbe, 6 metre genişliğindeki kemerlerle birbirine bağlanan 8 büyük payeye oturur. Mimar Sinan’ın inşa ettiği 8 dayanaklı cami planının en başarılı örneğidir. Caminin 3.80 cm çapında, 70,89 m yüksekliğindeki üçer şerefeli dört zarif minaresi vardır. Giriş yönündeki her şerefeye ayrı ayrı yollardan, diğer ikisine ise tek yolla çıkılmaktadır.<br />
+   Cami, mimari özelliklerinin erişilmezliği yanında taş, mermer, çini, ahşap ve sedef gibi süsleme özellikleriyle de son derece önemlidir. Mihrap ve mimberi mermer işçiliğinin başyapıtlarındandır. Ortasındaki 12 mermer sütuna oturan müezzin mahveli, altın varaklı edirnekari kalem işleriyle klasik dönemin en güzel örneklerindendir. Yapının çini süslemelerinin, Osmanlı ve dünya sanatında ayrı bir yeri vardır. 16. yy. çiniciliğinin en güzel örnekleri olan bu çiniler, sıratlı tekniğinde olup İznik’te yapılmıştır. <br />
+   Selimiye Camii’nin taş duvarlarla çevrili geniş dış avlusunda, Dar-ül- Sübyan, Dar-ül-Kurra ve Dar-ül-Hadis yapıları bulunmaktadır. Bu yapılardan Dar-ül-Hadis bölümü Türk-İslam eserleri müzesi olarak kullanılmaktadır. Dar-ül Kura bölümü ise Vakıf Eserleri müzesidir. Cami terasının altında yer alan Arasta çarşısı, III.Murat zamanında Selimiye’ye gelir sağlaması amacıyla yaptırılmıştır. İlk sırası Mimar Sinan tarafından yapılmış, daha sonra Mimar Davut Ağa tamamlamıştır.<br />
+   Edirne Selimiye Camii ve Külliyesi, UNESCO Dünya Miras Komitesi’nin 19-29.06.2011 tarihleri arasında gerçekleştirilen 35. Dönem Toplantısında alınan 35 COM 8B.37 sayılı karar ile 1. ve 4. kriterler kapsamında kültürel varlık olarak Dünya Miras Listesi’ne dahil edilmiştir.
+        </div>
+        <div className={styles.container_image}>
+          <img src={resim} />
+        </div>
+        </div>
+        <div style={{display:"flex",flexDirection:"row",justifyContent:"space-evenly"}}>
+        <div className={styles.container_image}>
+          <img src={resim} />
+        </div>
+        <div className={styles.container_text} >
+        Mimar Sinan‘ın 80 yaşında inşa ettiği ve “ustalık eserim“ diye nitelediği yapıt olan Selimiye Camii, Osmanlı-Türk mimarlık tarihinin olduğu kadar, dünya mimarlık tarihinin de başyapıtları arasında gösterilmektedir. Edirnenin ve aynı zamanda Osmanlı Döneminin simgesi olan cami; 1569-1575 yılları arasında II. Selim’in emriyle yaptırılmıştır. Çok uzaklardan dört minaresi ile göze çarpan yapı, kurulduğu yerin seçimiyle Mimar Sinan’ın aynı zamanda usta bir şehircilik uzmanı olduğunu da gösterir. Kesme taştan yapılan cami iç bölümüyle 1620 metrekare, tümüyle ise toplam 2475 metrekarelik bir alanı kaplamaktadır. Selimiye Camii, yerden yüksekliği 43.28 metre olan 31.30 metre çapındaki kubbesiyle ilgi çeker.<br />    
+   Kubbe, 6 metre genişliğindeki kemerlerle birbirine bağlanan 8 büyük payeye oturur. Mimar Sinan’ın inşa ettiği 8 dayanaklı cami planının en başarılı örneğidir. Caminin 3.80 cm çapında, 70,89 m yüksekliğindeki üçer şerefeli dört zarif minaresi vardır. Giriş yönündeki her şerefeye ayrı ayrı yollardan, diğer ikisine ise tek yolla çıkılmaktadır.<br />
+   Cami, mimari özelliklerinin erişilmezliği yanında taş, mermer, çini, ahşap ve sedef gibi süsleme özellikleriyle de son derece önemlidir. Mihrap ve mimberi mermer işçiliğinin başyapıtlarındandır. Ortasındaki 12 mermer sütuna oturan müezzin mahveli, altın varaklı edirnekari kalem işleriyle klasik dönemin en güzel örneklerindendir. Yapının çini süslemelerinin, Osmanlı ve dünya sanatında ayrı bir yeri vardır. 16. yy. çiniciliğinin en güzel örnekleri olan bu çiniler, sıratlı tekniğinde olup İznik’te yapılmıştır. <br />
+   Selimiye Camii’nin taş duvarlarla çevrili geniş dış avlusunda, Dar-ül- Sübyan, Dar-ül-Kurra ve Dar-ül-Hadis yapıları bulunmaktadır. Bu yapılardan Dar-ül-Hadis bölümü Türk-İslam eserleri müzesi olarak kullanılmaktadır. Dar-ül Kura bölümü ise Vakıf Eserleri müzesidir. Cami terasının altında yer alan Arasta çarşısı, III.Murat zamanında Selimiye’ye gelir sağlaması amacıyla yaptırılmıştır. İlk sırası Mimar Sinan tarafından yapılmış, daha sonra Mimar Davut Ağa tamamlamıştır.<br />
+   Edirne Selimiye Camii ve Külliyesi, UNESCO Dünya Miras Komitesi’nin 19-29.06.2011 tarihleri arasında gerçekleştirilen 35. Dönem Toplantısında alınan 35 COM 8B.37 sayılı karar ile 1. ve 4. kriterler kapsamında kültürel varlık olarak Dünya Miras Listesi’ne dahil edilmiştir.
+        </div>
+        
+        </div>
       </div>
     </div>
   );
