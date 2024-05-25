d499296d66befcae5653b00c1f6acd13d10786ca
Yunus Emre Gürdoğan <yunusemregurdogan0@gmail.com>
2024-05-25 17:50:49 +0300

    Gezi Planı sayfası taslağı oluşturuldu

--- src/pages/plan/Plan.jsx
+++ src/pages/plan/Plan.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import "./Plan.css";
 function Plan() {
   return (
-    <div>
-      Plan
+    <div className="body">
+      <div className="container">
+        <div className="card">
+          <div className="first-content">
+            <span>First</span>
+          </div>
+          <div className="second-content">
+            <a href="https://www.google.com/maps/dir//Meydan,+Selimiye+Cami,+Edirne+Merkez%2FEdirne/Karaa%C4%9Fa%C3%A7,+Edirne+Merkez%2FEdirne/" target="_blank" rel="noreferrer" ><span>Second</span></a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="first-content">
+            <span>First</span>
+          </div>
+          <div className="second-content">
+            <span>Second</span>
+          </div>
+        </div>
+        <div className="card">
+          <div className="first-content">
+            <span>First</span>
+          </div>
+          <div className="second-content">
+            <span>Second</span>
+          </div>
+        </div>
+        <div className="card">
+          <div className="first-content">
+            <span>First</span>
+          </div>
+          <div className="second-content">
+            <span>Second</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
