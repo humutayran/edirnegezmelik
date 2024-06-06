7b5634f26f8212d16b9d80ee8c14017d4b1b84b8
Hüseyin Umut Reyhanoğlu <h.umutayran@gmail.com>
2024-06-06 05:06:06 +0300

    yiyecek sayfasi card tasarimi anahatlari olusturuldu animasyonu tamamlandi

--- src/pages/yiyecek/Yiyecek.jsx
+++ src/pages/yiyecek/Yiyecek.jsx
@@ -1,17 +1,60 @@
 import React from "react";
 import "./Yiyecek.css";
+import image1 from "../../images/tava-ciger.webp";
+import image2 from "../../images/kavala-kurabiyesi.jpg";
+
 function Yiyecek() {
   return (
-    <div>
+    <div className="yoreselLezzetlerContainer">
       <div className="yoreselLezzetler">
-        <div className="yiyecek">
-          <h1 className="title">Bunları Yemeden Gitmeyin!</h1>
-          <h2 className="explanation">Edirne&#39;nin zengin tarihini ve kültürel mirasını keşfederken, lezzetli yöresel yemeklerini tatmayı unutmayın. Edirne mutfağının en özel ve meşhur yemeklerini sizin için derledik. Şimdi bu lezzetleri keşfetmeye</h2>
+        <div className="yiyecekResimContainer">
+          <div className="yiyecek">
+            <h1 className="foodName">Tava Ciğeri – Yaprak Ciğer</h1>
+            <h2 className="foodExplanation">
+              Edirne&#39;nin en meşhur lezzetlerinden biri olan tava ciğeri, ince ince dilimlenmiş ve kızartılmış yaprak ciğer olarak servis edilir. Yanında acı biber ve kızarmış ekmek ile sunulur.
+            </h2>
+          </div>
+          <div className="imageContainer">
+            <img src={image1} alt="Edirne" />
+          </div>
+        </div>
+        <div className="yiyecekResimContainer">
+          <div className="yiyecek">
+            <h1 className="foodName">Kavala kurabiyesi</h1>
+            <h2 className="foodExplanation">
+              Un, badem ve tereyağının mükemmel birleşimi olan kavala kurabiyesi, ağızda dağılan yapısıyla oldukça sevilen bir tatlıdır.
+            </h2>
+          </div>
+          <div className="imageContainer">
+            <img src={image2} alt="Edirne" />
+          </div>
+        </div>
+        <div className="yiyecekResimContainer">
+          <div className="yiyecek">
+            <h1 className="foodName">Tava Ciğeri – Yaprak Ciğer</h1>
+            <h2 className="foodExplanation">
+              Edirne&#39;nin en meşhur lezzetlerinden biri olan tava ciğeri, ince ince dilimlenmiş ve kızartılmış yaprak ciğer olarak servis edilir. Yanında acı biber ve kızarmış ekmek ile sunulur.
+            </h2>
+          </div>
+          <div className="imageContainer">
+            <img src={image1} alt="Edirne" />
+          </div>
+        </div>
+        <div className="yiyecekResimContainer">
+          <div className="yiyecek">
+            <h1 className="foodName">Kavala kurabiyesi</h1>
+            <h2 className="foodExplanation">
+              Un, badem ve tereyağının mükemmel birleşimi olan kavala kurabiyesi, ağızda dağılan yapısıyla oldukça sevilen bir tatlıdır.
+            </h2>
+          </div>
+          <div className="imageContainer">
+            <img src={image2} alt="Edirne" />
+          </div>
         </div>
       </div>
     </div>
-  )
-
+  );
 }
 
 export default Yiyecek;
+
